08bfd26e2f9ff25762a58a52ccb8dc6374cb7d85
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-10-07 11:41:48 +0200

    Fixed home page visuals

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,15 @@
 import { Link } from "react-router-dom";
-import leftBrain from "../images/glasses1l.png";
-import rightBrain from "../images/glasses1r.png";
-import leftGlass from "../images/glasses0l.png";
-import rightGlass from "../images/glasses0r.png";
 
 function HomePage() {
   return (
     <div id="home">
       <div id="container">
-        <div id="left-div">
-          <Link to="/coding-projects">
-            <img className="brain" src={leftBrain} alt="left-brain" />
-          </Link>
-          <img className="glass" src={leftGlass} alt="left-glass" />
-        </div>
-        <div id="right-div">
-          <Link to="/design-projects">
-            <img className="brain" src={rightBrain} alt="right-brain" />
-          </Link>
-          <img className="glass" src={rightGlass} alt="right-glass" />
-        </div>
+        <Link to="/coding-projects">
+          <div id="left-div"></div>
+        </Link>
+        <Link to="/design-projects">
+          <div id="right-div"></div>
+        </Link>
       </div>
     </div>
   );
